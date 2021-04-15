635987ac62400f553394555e70c2b55572e0a254
f2010t2149 <82414274+f2010t2149@users.noreply.github.com>
2021-04-15 05:13:46 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function init() {
-	var entryText = document.getElementById('entrybutton');
-	function myAlert({
-	var inText = document.getElementById("entryinput");
-	var outText = document.getElementById("textoutput");
-	alret("Daniel: " + inText.Value);
-	outText.innerHTML = inText.value;
-	}
-	entryText/addEventListener('click', myAlert);
-	}
-	window.addEventListener('load',init);
+var entryText = document.getElementtById('entrybutton');
+entryText.addEventListener('click',myAlert);
+function myAlret(){
+var inText = document.getElementById("entryinput");
+var outText = document.getElementById("textoutput");
+alert("Daniel: " + inText.Value);
+outText.innerHTML = inText.value;
+}
+}
+window.addEventListener('load',init);
